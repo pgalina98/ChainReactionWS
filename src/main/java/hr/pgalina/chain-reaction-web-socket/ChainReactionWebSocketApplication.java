73017067a8b3eab93ad1f54805da921db4ae3b00
Patrik Galina <patrik.galina@king-ict.hr>
2022-06-16 14:55:36 +0200

    chore: Initialize project 🚀

---
+++ src/main/java/hr/pgalina/chain-reaction-web-socket/ChainReactionWebSocketApplication.java
@@ -0,0 +1,13 @@
+package hr.pgalina.chainreactionwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainReactionWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainReactionWebSocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/pgalina/chainreactionwebsocket/ChainReactionWebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.pgalina.chainreactionwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainReactionWebSocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
